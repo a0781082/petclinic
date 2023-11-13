831867bb32e45e9fff1e00f0d9dc70047d3c529e
Animesh <12829119+SirAnimesh@users.noreply.github.com>
2023-11-13 16:29:07 +0000

    Update OwnerController.java

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.validation.Valid;
-
+// A useless change
 /**
  * @author Juergen Hoeller
  * @author Ken Krebs
